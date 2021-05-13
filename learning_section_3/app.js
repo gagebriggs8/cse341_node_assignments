e46f3f71858e33907796fc38cd83a4747595cd99
Gage Briggs <byron008@byui.edu>
2021-05-12 20:41:10 -0600

    help

--- learning_section_3/app.js
+++ learning_section_3/app.js
@@ -1,6 +1,25 @@
 var PORT = process.env.PORT || 3000;
 const express = require('express');
 
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+                        
+
 const bodyParser = require('body-parser');
 const path = require('path');
 //const expressHbs = require('express-handlebars');
